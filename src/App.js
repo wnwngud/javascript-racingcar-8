d5466ee1548c5e130a28a3b58ac40c60dda25576
wnwngud <blane777@naver.com>
2025-10-24 16:24:23 +0900

    feat(input): add user's input featuer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async run() {
+      let inputStr = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준  으로 구분)\n"
+      )
+      let numRepeats = await MissionUtils.Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      )
+  }
 }
 
 export default App;
